61de6b7652649d8960d2184cf5cb8ebca4aed733
Daijiro Wachi <daijiro.wachi@gmail.com>
2022-07-24 23:29:25 +0900

    initial commit

---
+++ scripts/build.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+const { Parser, transforms: { flatten } } = require('json2csv')
+
+const ORIGINAL = path.join(__dirname, '../original/messages_ja.json')
+const messages  = require(ORIGINAL)
+
+Object.keys(messages).forEach((item) => {
+  if (item === 'locale') return;
+  const json2csv = new Parser({
+    transforms: [flatten({
+      objects: true,
+      arrays: true
+    })]
+  })
+  const json = {[item]: messages[item]}
+  const csv = json2csv.parse(json)
+  fs.writeFileSync(path.join(__dirname, `../translations/${item}.csv`), csv)
+})
